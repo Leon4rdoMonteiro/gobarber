f7b9cc8bb45896f7d8cd89d450eba217f751e54e
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-13 23:33:37 -0300

    User - migration, model created + database connection

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Node!' }));
+routes.get('/', async(req, res) => {
+    const user = await User.create({
+        name: 'Leonardo Monteiro',
+        email: 'leonardomonteiro.sh@gmail.com',
+        password_hash: '1232gfd134134'
+    });
+    return res.json(user);
+});
 
 export default routes;
