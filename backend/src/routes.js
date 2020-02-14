4d3888073829f8512754cb8fe84907a7a998e34c
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-13 23:46:30 -0300

    User Controller added

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+// CONTROLLERS IMPORT
+import UserController from './app/controllers/UserCOntroller';
 
 const routes = new Router();
 
-routes.get('/', async(req, res) => {
-    const user = await User.create({
-        name: 'Leonardo Monteiro',
-        email: 'leonardomonteiro.sh@gmail.com',
-        password_hash: '1232gfd134134'
-    });
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
