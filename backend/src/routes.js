65fa83149c71265729c81afd6ba0fbaf8fec945e
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-19 00:13:57 -0300

    some updates on database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
 // CONTROLLERS IMPORT
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
